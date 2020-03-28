e48ed088339e59838bcc8220019f4cb322e7619c
Fabio Tavares Oliveira <fabiotavares19@hotmail.com>
2020-03-28 10:05:31 -0300

    Update script.js

--- JSBasics/JSconditionals/script.js
+++ JSBasics/JSconditionals/script.js
@@ -1,4 +1,4 @@
-/* First exercise in order to pratice .js syntax using conditionals and comparators to
+/* 1.) First exercise in order to pratice .js syntax using conditionals and comparators to
    simply receive grades from students and check for approval
 */
 
@@ -19,7 +19,7 @@ else{
     console.log('Both students failed exam')
 }
 
-/*Next one, calculate and store the BMI of the two students and 
+/* 2.) Next one, calculate and store the BMI of the two students and 
 compare using a boolean type
 */
 var hasHigherBMI = true;
@@ -33,3 +33,36 @@ var johnBMI = calculateBMI(johnMass,johnHeight);
 var nickBMI = calculateBMI(nickMass,nickHeight);
 hasHigherBMI = (johnBMI > nickBMI);
 console.log(hasHigherBMI);
+
+/* 3.) Exercise manipulating sample arrays and their size and dinamically using
+those values in calculating the average of each student and again use of conditinal
+to define the winner
+*/ 
+
+
+var johnScores = [89,120,103];
+var nickScores = [116,94,123];
+
+var johnAvgScore = 0;
+var nickAvgScore = 0;
+
+for(var i = 0; i < johnScores.length;i++){
+    johnAvgScore += johnScores[i];
+}
+johnAvgScore = (johnAvgScore/johnScores.length);
+console.log(johnAvgScore);
+
+
+for(var i = 0; i < nickScores.length;i++){
+    johnAvgScore += nickScores[i];    
+}
+nickAvgScore = (johnAvgScore/nickScores.length);
+console.log(nickAvgScore);
+
+if(johnAvgScore > nickAvgScore){
+    console.log('John wins');
+}
+else if(johnAvgScore < nickAvgScore){
+    console.log('Nick wins');
+}
+else{console.log("It's a draw");}
